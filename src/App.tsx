ef9cf73aea3c934703300342a02cf7337f6193d8
cHANGTEEZY <jagatgurung30@gmail.com>
2025-03-07 20:30:34 +0545

    cleaned cache and reupload

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,16 @@
 import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-  UNSAFE_DataRouterContext,
-  UNSAFE_DataRouterStateContext,
-} from "react-router";
 
 import { ThemeProvider } from "./components/theme-provider";
 import Home from "./pages/Home";
 import Error from "./components/error/Error";
 import Contact from "./pages/Contact";
 
-// Create router with future flags
-const router = {
-  future: {
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-  },
-};
-
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter {...router}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
